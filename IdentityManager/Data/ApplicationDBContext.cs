a422b0405ec0dad9579d7032c44c68ed3b8d6fc9
shamsisadr01 <shamsisadr01@gmail.com>
2025-08-10 10:44:44 +0330

    Configure DbContext in Program Class File

---
+++ IdentityManager/Data/ApplicationDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityManager.Data;
+
+public class ApplicationDBContext : IdentityDbContext
+{
+    public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+        : base(options)
+    {
+    }
+}

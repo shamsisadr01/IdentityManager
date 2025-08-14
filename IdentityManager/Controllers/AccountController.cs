4ba8a907f93d3ecd30a1aa500c921e4dfd937145
shamsisadr01 <shamsisadr01@gmail.com>
2025-08-14 11:41:04 +0330

    Reset Password in Action

--- IdentityManager/Controllers/AccountController.cs
+++ IdentityManager/Controllers/AccountController.cs
@@ -128,15 +128,30 @@ namespace IdentityManager.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult ResetPassword(ResetPasswordViewModel model)
+        public async Task<IActionResult> ResetPassword(ResetPasswordViewModel model)
         {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+                if (user == null)
+                {
+                    return RedirectToAction(nameof(ResetPasswordConfirmation));
+                }
+
+                var result = await _userManager.ResetPasswordAsync(user, model.Code, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction(nameof(ResetPasswordConfirmation));
+                }
+                AddErrors(result);
+            }
             return View();
         }
 
         [HttpGet]
-        public IActionResult ResetPasswordConfirmation(string code = null)
+        public IActionResult ResetPasswordConfirmation()
         {
-            return code == null ? View("Error") : View();
+            return  View();
         }
 
         [HttpPost]
@@ -160,6 +175,7 @@ namespace IdentityManager.Controllers
                 await _emailSender.SendEmailAsync("Shams",model.Email, "Reset Password - Identity Manager",
                     $"Please reset your password by clicking here: <a href='{callbackurl}'>link</a>");
 
+                return RedirectToAction(nameof(ForgotPasswordConfirmation));
             }
             return View(model);
         }
